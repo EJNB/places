b5fedbca6577f459b00fbebb5e761f199e630da8
javier <javiernieto1989@gmail.com>
2020-03-22 12:32:37 -0400

    Se implemeta la verificacion de token en las url q no son GET y tampoco hacen match con sessions y users

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser'); /*body-parser es una libreria q va a ser lectura del cuerpo de la peticion */
+/*Este middleware hace dos cosas, 1ro Valida el jwt y si es valido => genera una prop user dentro de req
+* en donde se almacena toda la information q hayamos procesado para el jwt. si es invalido bloquea el flujo de las peticiones*/
+const jwtMiddleware = require('express-jwt');
+const secrets = require('./config/secret');
 // const path = require('path');
 // const favicon = require('serve-favicon');
 // const logger = require('morgan');
@@ -19,7 +23,11 @@ const sessionsRouter = require('./routes/session.route');
 // app.use(logger('dev'));
 /*Configuracion del middelware body-parser, estas dos opciones dependen del tipo de peticion q estamos recibiendo*/
 app.use(bodyParser.json({}));//esta nos permitira leer las q vienen en formato raw
-app.use(bodyParser.urlencoded({extend: false}));//esta nos permitira leer las peticiones q vienen en formato x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extend: false}));//esta nos permitira leer las peticiones q vienen en formato x-www-form-urlencoded.
+app.use(
+    jwtMiddleware({secret: secrets.jwtSecret})
+        .unless({path: ['/sessions', '/users'], method: 'GET'})// Asi le estamos diciendo q excluya estas rutas.
+);
 // app.use(express.static(path.json(__dirname, 'public')));
 
 app.use('/places', placesRouter);
